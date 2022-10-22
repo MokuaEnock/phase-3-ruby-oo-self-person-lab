d9b5bc3f02de585c9d9f02c7f55ecb7389640f24
MokuaEnock <enock.mokua@student.moringaschool.com>
2022-10-22 12:01:54 +0300

    update

--- lib/person.rb
+++ lib/person.rb
@@ -2,14 +2,12 @@
 class Person
   attr_accessor :bank_account
   attr_reader :name, :happiness, :hygiene
-
   def initialize(name)
     @name = name
     @bank_account = 25
     @happiness = 8
     @hygiene = 8
   end
-
   def happiness=(num)
     @happiness =  if num > 10
                     10
@@ -18,11 +16,7 @@ class Person
                   else
                     num
                   end
-
-    # with clamp
-    # @happiness = num.clamp(0, 10)
   end
-
   def hygiene=(num)
     @hygiene =  if num > 10
                   10
@@ -31,41 +25,31 @@ class Person
                 else
                   num
                 end
-
-    # with clamp
-    # @hygiene = num.clamp(0, 10)
   end
-
   def clean?
     self.hygiene > 7
   end
-
   def happy?
     self.happiness > 7
   end
-
   def get_paid(amount)
     self.bank_account += amount
     "all about the benjamins"
   end
-
   def take_bath
     self.hygiene += 4
     "♪ Rub-a-dub just relaxing in the tub ♫"
   end
-
   def work_out
     self.happiness += 2
     self.hygiene -= 3
     "♪ another one bites the dust ♫"
   end
-
   def call_friend(friend)
     self.happiness += 3
     friend.happiness += 3
     "Hi #{friend.name}! It's #{self.name}. How are you?"
   end
-
   def start_conversation(friend, topic)
     case topic
     when "politics"
@@ -78,6 +62,5 @@ class Person
       "blah blah blah blah blah"
     end
   end
-
 end
 
